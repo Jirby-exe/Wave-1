eee9e50ede32851566e7a2c61926172ca6a7ca5e
Jordan Irby <jordan.a.irby@gmail.com>
2026-03-16 15:52:18 -0400

    fixing merge conflict -,

--- contact-form-a11y/src/App.tsx
+++ contact-form-a11y/src/App.tsx
@@ -2,12 +2,12 @@
 import ContactForm from "./components/ContactForm";
 
 function App() {
-  return(
+  return (
     <div>
       <h1>Contact Us</h1>
       <ContactForm />
     </div>
-  )
+  );
 }
 //need to move button state logic from here into ContactForm.tsx
 
